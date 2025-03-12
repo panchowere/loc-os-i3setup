2ba77eebe0217bfe3723a108b3cb908be875e44d
Francisco Alvarado <francisco.alvarado.co@gmail.com>
2025-03-12 18:14:06 -0300

    Add gtk config, new i3blocks config (based on endeavourOS), installation of xclip for copy and paste in neovim, change the wallpapers dir( home/user/pictures/wallpapers/ to /home/user/.wallpapers/) and replace kitty terminal with lxterminal (add coloscheme to lxterminal), fix errors in i3 config file, add connman-gtk in startup, change the theme and icon theme

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-echo -e "this script will install the following programs: \n i3wm \n i3block \n kitty \n rofi \n picom \n feh \n Xdg-user-dir \n lxappearance \n And compile the following programs:\n Neovim \n \033[33mWARNING:\033[0m This script will modify configuration files"
+echo -e "this script will install the following programs: \n i3wm \n i3block \n lxterminal \n rofi \n picom \n feh \n Xdg-user-dir \n lxappearance \n lxpolkit \n ETC... \n And compile the following programs:\n Neovim \n \033[33mWARNING:\033[0m This script will modify configuration files"
 echo "Do you agree? (Y/N):" & read YN 
 function dotFiles()
 {
@@ -8,14 +8,28 @@ function dotFiles()
 	mkdir -p  /home/$USER/.fonts/
 	cp -r  ./fonts/* /home/$USER/.fonts/
 	
-	echo "Copying configuration of nvim, rofi and kitty to .config"
+	echo "Copying icons and themes"
+	mkdir -p /home/$USER/.themes
+	cp -r ./themes/* /home/$USER/.themes
+	
+	mkdir -p /home/$USER/.icons
+	cp -r ./icons/* /home/$USER/.icons
+
+	mkdir -p /home/$USER/.config/gtk3/
+	cp -r ./gtk3/* /home/$USER/.config/gtk3/
+
+	echo "Copying configuration of nvim, rofi and lxterminal to .config"
 	mkdir -p /home/$USER/.config/
 	cp -r ./nvim/ /home/$USER/.config/
 	cp -r ./rofi/ /home/$USER/.config/
-	
-	echo "Copying Wallpapers folder to "$(xdg-user-dir PICTURES)""
-	mkdir -p "$(xdg-user-dir PICTURES)"
-	cp -r ./wallpapers/ "$(xdg-user-dir PICTURES)"
+	cp -r ./lxterminal /home/$USER/.config/
+	echo "Copying configuration of nvim, rofi and kitty to .config"
+	mkdir -p /home/$USER/.config
+	cp -r ./i3/ /home/$USER/.config/
+
+	echo "Copying Wallpapers folder to .wallpapers"
+	mkdir -p /home/$USER/.wallpapers/
+	cp -r ./wallpapers/* /home/$USER/.wallpapers/
 }
 
 function nvimCompile
@@ -36,9 +50,9 @@ echo "The folder "$i3setupDir" will self-destruct \n Do you agree? (Y/N)"
 read selfdestroy
 
 if [[  $selfdestroy == "y" || $selfdestroy == "Y" ]]
-	then cd ..
+	then cd .. && cd ..
 	rm -rf "$i3setupDir"
-	echo "ok"
+	echo "dir deleted!"
 else
     echo "bye bye~"
 fi
@@ -46,7 +60,7 @@ fi
 }
 
 if [[ $YN == "y" || $YN == "Y" ]]
-	then sudo apt install i3-wm rofi i3blocks rofi picom feh xdg-user-dirs kitty build-essential && xdg-user-dirs-update && dotFiles && sudo apt remove neovim neovim-runtime -y && nvimCompile && delete-folder && echo "ok, Bye!" && exit 1		
+	then sudo apt install i3-wm rofi i3blocks rofi picom feh xdg-user-dirs lxterminal lxpolkit lxappearance acpi alsa-utils xclip sysstat build-essential && xdg-user-dirs-update && dotFiles && sudo apt remove neovim neovim-runtime -y && nvimCompile && delete-folder && echo "ok, Bye!" && exit 1		
 elif [[ $YN == "n" || $YN == "N" ]]
 	then echo "ok, Bye!" && exit 1 
 else 
--- nvim/init.vim
+++ nvim/init.vim
@@ -1,2 +1,2 @@
-set clipboard=unnamed
+set clipboard=unnamedplus
 set number
